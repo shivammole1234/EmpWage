4f07daa561bef098ce942510b5201e20f2125506
shivam mole <shivmole1713@gmail.com>
2024-09-09 14:06:11 +0530

    test case 6

--- src/main/java/bridgelabz/empwage/oops/EmpWageBuilderUC.java
+++ src/main/java/bridgelabz/empwage/oops/EmpWageBuilderUC.java
@@ -7,6 +7,7 @@ public class EmpWageBuilderUC {
     public static final int is_part_time=1;
     public static final int emp_Rate_per_hour=20;
     public static  final  int num_of_working_days=20;
+    public static final int max_working_hr_month=100;
 
 
     public  static void main(String[] args){
@@ -15,37 +16,32 @@ public class EmpWageBuilderUC {
 
         //varibales
         int emp_hrs=0;
+        int total_emp_hr=0;
+        int total_working_days=0;
         int emp_wage=0;
         int total_emp_wage=0;
 
-        for(int i=0;i<num_of_working_days;i++)
-        {
-            int empCheck = (int) (Math.floor(Math.random() *10)%3);
-            switch (empCheck){
+        while(total_emp_hr<=max_working_hr_month &&
+                total_working_days<num_of_working_days){
+
+            total_working_days++;
+            int empCheck=(int) Math.floor(Math.random()*10) % 3;
+
+            switch(empCheck){
                 case is_part_time:
                     emp_hrs=4;
                     break;
                 case is_full_time:
-                    emp_hrs =8;
+                    emp_hrs=8;
                     break;
                 default:
                     emp_hrs=0;
             }
-            emp_wage=emp_hrs*emp_Rate_per_hour;
-            total_emp_wage +=emp_wage;
+            total_emp_hr+=emp_hrs;
+            System.out.println("Days :- " + total_working_days + " ,Emp hour :- "+emp_hrs);
 
-            System.out.println("emp wage individual day is :- "+emp_wage);
         }
-
-//        if(empCheck == is_full_time)
-//            emp_hrs=8;
-//        else if(is_part_time ==empCheck)
-//            emp_hrs=4;
-//        else
-//            emp_hrs=0;
-
-
-
+        total_emp_wage =total_emp_hr*emp_Rate_per_hour;
 
         System.out.println("total Emp Wage is :-"+total_emp_wage);
 
