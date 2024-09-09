74c021203fc295f1be42197d1bf171627a656bb2
shivam mole <shivmole1713@gmail.com>
2024-09-09 12:51:27 +0530

    Teat Case 3

--- src/main/java/bridgelabz/empwage/oops/EmpWageBuilderUC.java
+++ src/main/java/bridgelabz/empwage/oops/EmpWageBuilderUC.java
@@ -1,20 +1,23 @@
 package bridgelabz.empwage.oops;
 
-public class EmpWageBuilderUC1 {
+public class EmpWageBuilderUC {
     public  static void main(String[] args){
 
         //constat
-        int is_full_time=0;
+        int is_full_time=2;
+        int is_part_time=1;
         int emp_Rate_per_hour=20;
 
         //varibales
         int emp_hrs=0;
         int emp_wage=0;
 
-        double empCheck =Math.floor(Math.random() *10)%2;
+        double empCheck =Math.floor(Math.random() *10)%3;
 
         if(empCheck == is_full_time)
             emp_hrs=8;
+        else if(is_part_time ==empCheck)
+            emp_hrs=4;
         else
             emp_hrs=0;
 
