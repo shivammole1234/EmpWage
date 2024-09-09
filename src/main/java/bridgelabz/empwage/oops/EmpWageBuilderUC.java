9701642083db0da9767bb835dabdb9f9126607b5
shivam mole <shivmole1713@gmail.com>
2024-09-09 13:48:14 +0530

    test case 4

--- src/main/java/bridgelabz/empwage/oops/EmpWageBuilderUC.java
+++ src/main/java/bridgelabz/empwage/oops/EmpWageBuilderUC.java
@@ -6,6 +6,8 @@ public class EmpWageBuilderUC {
     public static final int is_full_time=2;
     public static final int is_part_time=1;
     public static final int emp_Rate_per_hour=20;
+    public static  final  int num_of_working_days=20;
+
 
     public  static void main(String[] args){
 
@@ -14,8 +16,26 @@ public class EmpWageBuilderUC {
         //varibales
         int emp_hrs=0;
         int emp_wage=0;
+        int total_emp_wage=0;
 
-        int empCheck = (int) (Math.floor(Math.random() *10)%3);
+        for(int i=0;i<num_of_working_days;i++)
+        {
+            int empCheck = (int) (Math.floor(Math.random() *10)%3);
+            switch (empCheck){
+                case is_part_time:
+                    emp_hrs=4;
+                    break;
+                case is_full_time:
+                    emp_hrs =8;
+                    break;
+                default:
+                    emp_hrs=0;
+            }
+            emp_wage=emp_hrs*emp_Rate_per_hour;
+            total_emp_wage +=emp_wage;
+
+            System.out.println("emp wage individual day is :- "+emp_wage);
+        }
 
 //        if(empCheck == is_full_time)
 //            emp_hrs=8;
@@ -24,19 +44,10 @@ public class EmpWageBuilderUC {
 //        else
 //            emp_hrs=0;
 
-        switch (empCheck){
-            case is_part_time:
-                emp_hrs=4;
-                break;
-            case is_full_time:
-                emp_hrs =8;
-                break;
-            default:
-                emp_hrs=0;
-        }
 
-        emp_wage=emp_hrs*emp_Rate_per_hour;
-        System.out.println("Emp Wage is :-"+emp_wage);
+
+
+        System.out.println("total Emp Wage is :-"+total_emp_wage);
 
     }
 }
