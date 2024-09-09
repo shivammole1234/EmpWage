28ab8b5777c44e8e53d64a32a1cd223702d543fb
shivam mole <shivmole1713@gmail.com>
2024-09-09 12:47:14 +0530

    Test Case 2

--- src/main/java/bridgelabz/empwage/oops/EmpWageBuilderUC1.java
+++ src/main/java/bridgelabz/empwage/oops/EmpWageBuilderUC1.java
@@ -3,14 +3,23 @@ package bridgelabz.empwage.oops;
 public class EmpWageBuilderUC1 {
     public  static void main(String[] args){
 
+        //constat
         int is_full_time=0;
+        int emp_Rate_per_hour=20;
+
+        //varibales
+        int emp_hrs=0;
+        int emp_wage=0;
 
         double empCheck =Math.floor(Math.random() *10)%2;
 
         if(empCheck == is_full_time)
-            System.out.println("Employee is present");
+            emp_hrs=8;
         else
-            System.out.println("Employee is abscent");
+            emp_hrs=0;
+
+        emp_wage=emp_hrs*emp_Rate_per_hour;
+        System.out.println("Emp Wage is :-"+emp_wage);
 
     }
 }
