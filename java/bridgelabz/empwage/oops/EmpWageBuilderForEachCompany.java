353a0286273664560b88e1ef8287298e8c450d5b
Shivam Mole <shivammole1713@gmail.com>
2024-09-12 14:22:52 +0530

    Emp Wage builder as per company

---
+++ java/bridgelabz/empwage/oops/EmpWageBuilderForEachCompany.java
@@ -0,0 +1,66 @@
+package bridgelabz.empwage.oops;
+
+public class EmpWageBuilderForEachCompany {
+  final int is_part_time=1;
+  final int is_full_time=2;
+  final String company_name;
+  final int max_working_days;
+  final int max_working_hrs;
+  final int wage_per_hr;
+  int total_wage;
+  int working_hrs;
+  int working_day;
+  int total_working_hr;
+  EmpWageBuilderForEachCompany
+          (String company_name,int wage_per_hr,int max_working_days,int max_working_hrs)
+  {
+      this.company_name=company_name;
+      this.wage_per_hr=wage_per_hr;
+      this.max_working_hrs=max_working_hrs;
+      this.max_working_days=max_working_days;
+      total_wage=0;
+      working_hrs=0;
+      working_day=0;
+      total_working_hr=0;
+
+  }
+
+  void calculate_total_wage(){
+      System.out.println("calculating the eamployee wage for the specific comoany");
+
+      while (total_working_hr<max_working_hrs && working_day<max_working_days)
+      {
+          working_day++;
+          int emp_type=(int)(Math.random()*100)%3;
+
+          switch (emp_type){
+              case is_full_time:
+                  working_hrs=8;
+                  break;
+              case is_part_time:
+                  working_hrs=4;
+                  break;
+              default:
+                  working_hrs=0;
+                  break;
+          }
+          int wage=working_hrs*wage_per_hr;
+          total_wage+=wage;
+          total_working_hr+=working_hrs;
+
+
+      }
+      System.out.println("printing the details of company");
+      System.out.println("Company Name :-"+company_name+ "\n Max Working Days :- "
+              +working_day+"\n Max Working hour:- "+max_working_hrs+
+              " \n Wage per Hour:- "+wage_per_hr+" " +"\ntotal working hours:-" +total_working_hr+" \n Total wage of employee :- "
+              +total_wage);
+  }
+
+    public static void main(String[] args) {
+    EmpWageBuilderForEachCompany amazon=
+            new EmpWageBuilderForEachCompany("amazon",12,22,69);
+    amazon.calculate_total_wage();
+
+    }
+}
--- java/bridgelabz/empwage/oops/EmpWageBuilderUC.java
+++ java/bridgelabz/empwage/oops/EmpWageBuilderUC.java
@@ -2,50 +2,52 @@ package bridgelabz.empwage.oops;
 
 public class EmpWageBuilderUC {
 
+        public static void calculateTotalWage(String company, int emp_Rate_per_hour, int num_of_working_days, int max_working_hr_month)
+        {
+
+            final int is_full_time = 2;
+            final int is_part_time = 1;
+            int total_emp_wage = 0;
+            int total_emp_hr = 0;
+            int total_working_days=0;
+            int emp_hrs=0;
+            System.out.println("\n Details of "+company+ " employee");
+            System.out.println("Wage per hour "+emp_Rate_per_hour);
+            System.out.println("Maximum working hours "+max_working_hr_month);
+            System.out.println("Maximum working days "+num_of_working_days);
 
 
-    public static void calculate_total_wage(){
-        //constat
-         final int is_full_time=2;
-         final int is_part_time=1;
-        final int emp_Rate_per_hour=20;
-          final  int num_of_working_days=20;
-         final int max_working_hr_month=100;
 
-        //varibales
-        int emp_hrs=0;
-        int total_emp_hr=0;
-        int total_working_days=0;
-        int emp_wage=0;
-        int total_emp_wage=0;
+            while(total_emp_hr<=max_working_hr_month &&
+                    total_working_days<num_of_working_days) {
 
-        while(total_emp_hr<=max_working_hr_month &&
-                total_working_days<num_of_working_days){
+                total_working_days++;
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-            total_working_days++;
-            int empCheck=(int) Math.floor(Math.random()*10) % 3;
+                switch (empCheck) {
+                    case is_part_time:
+                        emp_hrs = 4;
+                        break;
+                    case is_full_time:
+                        emp_hrs = 8;
+                        break;
+                    default:
+                        emp_hrs = 0;
+                }
+                total_emp_hr += emp_hrs;
+                System.out.println("Days :- " + total_working_days + " ,Emp hour :- " + emp_hrs);
 
-            switch(empCheck){
-                case is_part_time:
-                    emp_hrs=4;
-                    break;
-                case is_full_time:
-                    emp_hrs=8;
-                    break;
-                default:
-                    emp_hrs=0;
             }
-            total_emp_hr+=emp_hrs;
-            System.out.println("Days :- " + total_working_days + " ,Emp hour :- "+emp_hrs);
-
+            total_emp_wage =total_emp_hr*emp_Rate_per_hour;
+            System.out.println("Total wage for a month of "+ company+ " employee is " + total_emp_wage);
         }
-        total_emp_wage =total_emp_hr*emp_Rate_per_hour;
-
-        System.out.println("total Emp Wage is :-"+total_emp_wage);
-
+        public static void main( String[] args )
+        {
+            System.out.println( "Welcome to Employee Wage Computation Program" );
+            calculateTotalWage("Cogni", 20,16, 150);
+            calculateTotalWage("Capg",40,20,100);
+            calculateTotalWage("Nike",30,15,100);
+            calculateTotalWage("Sparx",20,20,200);
+            calculateTotalWage("HP", 25,25, 150);
+        }
     }
-
-    public  static void main(String[] args){
-        calculate_total_wage();
-    }
-}
